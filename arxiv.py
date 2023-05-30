d2fe2e332fcdc45675530915fa107d8dfbd92d6a
林方圆 <129135118+fynikky@users.noreply.github.com>
2023-05-30 09:58:37 +0800

    Update arxiv.py

--- arxiv.py
+++ arxiv.py
@@ -11,7 +11,8 @@ import json
 import datetime
 from tqdm import tqdm
 
-SERVERCHAN_API_KEY = os.environ.get("SERVERCHAN_API_KEY", None)
+#SERVERCHAN_API_KEY = os.environ.get("SERVERCHAN_API_KEY", None)
+SERVERCHAN_API_KEY = "SCT211589Tg5U9sGo6ubUxB5A3mwSdULZR"
 QUERY = os.environ.get('QUERY', 'cs.IR')
 LIMITS = os.environ.get('LIMITS', 3)
 CAIYUN_TOKEN = os.environ.get("CAIYUN_TOKEN", None)
